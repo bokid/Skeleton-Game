38a85096450e06ced6dbcf2bcb0c5407edb18149
Julian -Home <j.henry.b.orr@gmail.com>
2022-01-20 18:37:00 -0500

    Add Cinemachine to player and create confiner
    Cinemachine is a unity camera extension that can be found in the packagemanager. When creating a new camera object from Cinemachine, it takes control over the main camera. This commit adds Cinemachine to our project, and implements the game object "CM vcam1" to track the player within defined container game object "CameraContainer"

---
+++ platformKata/Assets/CameraManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject Cam1;
+    public GameObject Cam2;
+
+    public GameObject Trigger;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Cam1.SetActive(false);
+            Cam2.SetActive(true);
+        }
+
+    }
+}
